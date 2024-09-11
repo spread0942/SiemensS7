c118345fa1978a079562ca626a8b0361ee05cd8b
spread0942 <riccalira95@protonmail.com>
2024-09-11 11:43:12 +0200

    Simple SiemensS7 client

---
+++ SiemensTest/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using S7.Net;
+
+namespace SiemensTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// Example: https://angolodiwindows.com/2016/12/gestione-plc-con-s7-net-library-parte-uno/
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Plc _plc = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        // scrittura
+        private void btnRESetBit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_plc != null && _plc.IsConnected)
+                {
+                    string node = this.tbkWriteNode.Text;
+                    string value = this.tbkValoreToSend.Text;
+
+                    _plc.Write(node, value);
+                    this.WriteLog($"Write to node: {node}");
+                }
+            }
+            catch (Exception ex)
+            {
+                this.WriteLog(ex.Message);
+            }
+        }
+
+        // lettura
+        private void btnSetBit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_plc != null && _plc.IsConnected)
+                {
+                    DataType dataType = (DataType)this.cmboxDataType.SelectedItem;
+                    int db = int.Parse(this.tbkNumDB.Text);
+                    int startByteAddr = int.Parse(this.tbkStartAddr.Text);
+                    VarType varType = (VarType)this.cmboxNodeType.SelectedItem;
+                    int varCount = int.Parse(this.tbkVarCount.Text);
+                    byte bitAdr = byte.Parse(this.tbkBitCount.Text);
+
+                    var value = _plc.Read(dataType, db, startByteAddr, varType, varCount, bitAdr);
+                    this.WriteLog($"Read on {dataType}, db {db} the following value {value}");
+                }
+            }
+            catch (Exception ex)
+            {
+                this.outputTexBlock.Text = ex.Message;
+            }
+        }
+
+        // connessione
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (_plc != null && _plc.IsConnected)
+                {
+                    _plc.Close();
+                    this.btnConnect.Content = "Connect";
+                    this.btnSetBit.IsEnabled = false;
+                    this.btnRESetBit.IsEnabled = false;
+                    this.WriteLog($"Disconnected from {_plc.IP}");
+                }
+                else
+                {
+                    string ip = tbxImpostaVariabile.Text;
+                    short rack = short.Parse(this.Rack.Text);
+                    short slot = short.Parse(this.Slot.Text);
+                    CpuType cpuType;
+
+                    if ((bool)this.D.IsChecked)
+                    {
+                        cpuType = CpuType.S7200;
+                    }
+                    else if ((bool)this.T.IsChecked)
+                    {
+                        cpuType = CpuType.S7300;
+                    }
+                    else if ((bool)this.Q.IsChecked)
+                    {
+                        cpuType = CpuType.S7400;
+                    }
+                    else if ((bool)this.MC.IsChecked)
+                    {
+                        cpuType = CpuType.S71500;
+                    }
+                    else
+                    {
+                        cpuType = CpuType.S71200;
+                    }
+
+                    _plc = new Plc(cpuType, ip, rack, slot);
+                    _plc.Open();
+                    this.btnConnect.Content = "Disconnect";
+                    this.btnSetBit.IsEnabled = true;
+                    this.btnRESetBit.IsEnabled = true;
+                    this.WriteLog($"Connected from {_plc.IP}");
+                }
+            }
+            catch (Exception ex)
+            {
+                this.outputTexBlock.Text = ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// Write the log
+        /// </summary>
+        /// <param name="txt"></param>
+        private void WriteLog(string txt)
+        {
+            this.outputTexBlock.Text += string.Concat("\n[", DateTime.Now, "] - ", txt);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.cmboxNodeType.ItemsSource = Enum.GetValues(typeof(S7.Net.VarType)).Cast<S7.Net.VarType>();
+                this.cmboxNodeType.SelectedIndex = 0;
+                this.cmboxDataType.ItemsSource = Enum.GetValues(typeof(S7.Net.DataType)).Cast<S7.Net.DataType>();
+                this.cmboxDataType.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ SiemensTest/README.md
@@ -0,0 +1,16 @@
+﻿# Test Siemens
+
+Progetto con l'unico scopo di testare le librerie Siemens e la riuscita di interazione.
+
+Per comunicare con il protocollo Siemens dobbiamo avere la libreria S7netplus. In questo progetto è già attiva, ma per installare seguire i seguenti passaggi: `Project > Manage NuGet Packages..` e cerchiamo il pacchetto S7netplus. Di questo pacchetto troviamo diverse informazioni in [GitHub](https://github.com/S7NetPlus/s7netplus/wiki).
+
+Intanto per connettersi dobbiamo far parte del mondo Siemens, per farlo dobbiamo conntterci via **Ethernet**. Una volta collegati alla macchina individuaiamo i vari indirizzi IP, ce ne sono di diverso tipo, potrebbe esserci il router (in questo caso è 192.168.0.100), potrebbe esserci il PLC (in questo caso 192.168.0.101) ed invece dovrebbe esserci quello che interessa a noi e che ci farà leggere la parte di memoria (in questo caso 192.168.0.102).
+Come nel file MainWindows, andremo ad instanziare la nostra classe PLC, dove gli assegnamo il tipo di CPU (in questo caso è la S71200, consultabile da pdf), l'indirizzo IP (in questo caso 192.168.0.102) e per la CPU 1200 il rack è a 0 e stazione di posizione 1. Una volta creato il nostro oggetto, andiamo ad aprire la connessione con la macchina, se va a buon fine allora possiamo ad andare o leggere o scrivere le variabili ([Lista dei possibili errori](https://github.com/S7NetPlus/s7netplus/wiki#error-handling).
+
+Per la lettura dobbiamo utilizzare il metodo `S7.Read()`, abbiamo due versioni. Per quella che accetta una sola stringa di input dobbiamo seguire questa logica //<nome database>.<nome variabile>. Il nome database lo troviamo nel file pdf e nel nostro caso è DB100. Per le variabili la logica è la seguente, iniziare con DB, il terzo dato specificherà il tipo di dato da leggere (ad esempio se sarà W leggerà un Word, se sarà D leggerà Dint, se sarà X leggerà un solo bit all'interno di un 8bit, quindi un dato boleano e così via) nel nostro caso sarà X per i boleani e D per gli Int32. Ed infine specificheremo il nostro bite ed i suoi bit da leggere.
+Per esempio:
+
+* DB100.DBX0.1 >> significhe che nel database DB100, nella variabile boleanna nel spazio di memoria del bite 0, leggeremo il bit 1. Che nel nostro caso consiste nel "Stato Start Ciclo Continuo"
+* DB100.DBD22.0 >> significa che nel database DB100, nella variabile Dint (ovvero Int a 32 bit) leggeremo il bite 22. Per noi equivale al "Stato Macchina"
+
+Ricordarsi appunto che le variabili laborano con 8bit, con le variabili di tipo word potrebbero nascere dei problemi, perché per logiche vengono cambiati dei bit, ma non dovrebbe essere un problema nostro per il momento perché non abbiamo word.
